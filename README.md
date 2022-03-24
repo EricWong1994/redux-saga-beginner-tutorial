eb9af8b6fc3abd0052e1976a6cb6b3a34dc64188
后厂村小王 <48429995+EricWong1994@users.noreply.github.com>
2022-03-24 09:42:09 +0800

    学任何东西首先一定要去官网，是英文就去看英文，别tm再去掘金看入门文章了，真tm耽误事

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+# 学任何东西首先一定要去官网，是英文就去看英文，别tm再去掘金看入门文章了，真tm耽误事
 # redux-saga-beginner-tutorial
 Companion Repo for [Redux/Redux-saga beginner tutorial](https://github.com/redux-saga/redux-saga/blob/master/docs/introduction/BeginnerTutorial.md)
 
--- main.js
+++ main.js
@@ -3,18 +3,38 @@ import "@babel/polyfill"
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 
 import Counter from './Counter'
 import reducer from './reducers'
 
-const store = createStore(reducer)
+// const store = createStore(reducer)
 
 const action = type => store.dispatch({type})
+
+//...
+import { helloSaga } from './sagas'
+
+
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware)
+)
+sagaMiddleware.run(helloSaga)
+
+// const action = type => store.dispatch({type})
+
+const incre = () => {
+  action('INCREMENT');
+  // console.log(this.props)
+}
 
 function render() {
   ReactDOM.render(
     <Counter
       value={store.getState()}
+      // onIncrement={() => incre()}
       onIncrement={() => action('INCREMENT')}
       onDecrement={() => action('DECREMENT')} />,
     document.getElementById('root')
